ced158ae92dff2734b012ba7bc6b930530cedf82
Siham <90715354+SihamWeb@users.noreply.github.com>
2023-02-13 23:42:34 +0100

    Add files via upload

---
+++ eporfolio-en-ligne/certifications.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include'includes/header.html'; ?>
+              <div id="header">
+                <h2>Mes <span>Certifications</span></h2>
+                <div id="more">
+                  <p>Retrouvez sur cette page les certifications obtenues lors de mes formations, <br>
+                    j'enrichi cette page au fur et à mesure de mes formations dont certaines <br>
+                    sont en cours ou que je vais bientôt débuter.</p>
+                  <button><a href="#mes-certifs">Découvrez !</a></button>
+                  <button><a href="pdf/CV_Siham-Charef.pdf">Mon CV</a></button>
+                </div>
+              </div>
+        </header>
+      
+      <a id="mes-certifs"></a>
+      <div id="sect-certification">
+        
+        <div class="each-certif">
+            <img src="media/economie.jpg" alt="Logo" title="Logo" height="136" width="186">
+            <ul>
+            	<li><span>Nom</span> Baccalauréat ES</li>
+              	<li><span>Lieu</span> Lycée Robert Doisneau</li>
+                <li><span>Spécialités</span> Mathématiques</li>
+                <li><span>Année</span> 2019</li>
+            </ul>
+          	<div class="each-certif-btn">
+              <button>
+                <a href="pdf/certificat-baccalaureat">Y accéder</a>
+              </button>
+            </div>
+        </div>
+        
+        
+        <div class="each-certif">
+            <img src="media/webmaster.jpg" alt="Logo" title="Logo" height="136" width="186">
+            <ul>
+            	<li><span>Nom</span> Webmaster</li>
+                <li><span>Lieu</span> En Mode Formation</li>
+                <li><span>Durée</span> 4.5 jours intensifs</li>
+                <li><span>Année</span> 2021</li>
+            </ul>
+          	<div class="each-certif-btn">
+              <button>
+                <a href="pdf/certificat-webmaster">Y accéder</a>
+              </button>
+            </div>
+        </div>
+        
+        <div class="each-certif">
+            <img src="media/marketing.jpg" alt="Logo" title="Logo" height="136" width="186">
+            <ul>
+            	<li><span>Nom</span> Marketing digital</li>
+                <li><span>Lieu</span> En Mode Formation</li>
+                <li><span>Durée</span> 4.5 jours intensifs</li>
+                <li><span>Année</span> 2021</li>
+            </ul>
+            <div class="each-certif-btn">
+              <button>
+                <a href="pdf/certificat-marketing">Y accéder</a>
+              </button>
+            </div>
+        </div>
+        
+        <div class="each-certif">
+            <img src="media/webdesign.jpg" alt="Logo" title="Logo" height="136" width="186">
+            <ul>
+            	<li><span>Nom</span> Graphic Designer</li>
+                <li><span>Lieu</span> En Mode Formation</li>
+                <li><span>Durée</span> 4.5 jours intensifs</li>
+                <li><span>Année</span> 2021</li>
+            </ul>
+          	<div class="each-certif-btn">
+              <button>
+                <a href="pdf/certificat-webdesign">Y accéder</a>
+              </button>
+            </div>
+        </div>
+      
+      </div>
+        
+<?php include'includes/footer.html'; ?>
---
+++ eporfolio-en-ligne/index.php
@@ -0,0 +1,143 @@
+<?php include'includes/header.html'; ?>
+                <div id="header">
+                    <p>Bonjour, je suis</p>
+                    <h2>Charef <span>Siham</span></h2>
+                    <h3>Une étudiante en Webmaster et Métiers de l'Internet.</h3> 
+                	<div id="more">
+                      <p>Si vous voulez en savoir plus sur mon parcours et mes objectifs,<br />n'hésitez pas à cliquez sur le bouton ci-dessous.</p>
+                      <button><a href="#aboutme">Découvrez-moi !</a></button>
+                      <button><a href="pdf/CV_Siham-Charef.pdf" target="_blank" title="Mon CV">Mon CV</a></button>
+                  	</div>
+                </div>
+        </header>
+        
+        <a id="aboutme"></a>
+        <section id="about">
+            <div id="content">
+                <div class="about-img"><img src="media/about3.jpg" alt="Photo présentation" title="Photo présentation"></div>
+                <div id="about-text">
+                    <h3>A propos</h3>
+                    <h4>Développement web <span>& Graphisme</span></h4>
+                    <div>
+                        <p> <strong class="pink">Depuis mon plus jeune âge,</strong> j'ai eu l'envie de devenir : médecin, avocate, juge, commissaire aux comptes, et de nombreux autres métiers dont je tairais le nom.
+                            <br />Mais, le développement web et le graphisme, d'où m'est-elle venu cette attirance envers ces métiers de l'Internet ? J'ai toujours aimé l'informatique et le design. Je dessine depuis toujours 
+                            (même si ce sont seulement des petites maisons avec des individus dessinés en bâtons !)
+                            <br /><strong class="pink">A vrai dire,</strong> la crise sanitaire de la Covid a été une chose horrible pour certains mais a été mon déclencheur pour me rendre compte qu'il fallait que je change de direction professionnelle. Confinement et risque sanitaire ont été des causes qui m'ont permis de découvrir davantage le monde des réseaux sociaux et donc les métiers du web.
+                            <br /><strong class="pink">Entre entreprises de développement web, graphistes infépendants et rédacteurs web,</strong> j'ai pu ouvrir les yeux sur le monde de demain et être certaine que c'est cette voie qui m'intéresse : travail à domicile, informatique, design, liberté géographique, liberté temporelle ; tout ce qui me convient ! <br/>
+                            Siham CHAREF, étudiante en DEUST WMI à l'Université de Limoges.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+		
+		<section class="slider">
+          <div class="slide-track">
+            <div class="slide">
+              <img src="media/html-5.png" title="HTML5" alt="HTML5">
+            </div>
+            <div class="slide">
+              <img src="media/css.png" title="CSS" alt="CSS">
+            </div>
+            <div class="slide">
+              <img src="media/js.png" title="JS" alt="JS">
+            </div>
+            <div class="slide">
+              <img src="media/php.png" title="PHP" alt="PHP">
+            </div>
+            <div class="slide">
+              <img src="media/wordpress.png" title="Wordpress" alt="Wordpress">
+            </div>
+            <div class="slide">
+              <img src="media/symfony.png" title="Symfony" alt="Symfony">
+            </div>
+            <div class="slide">
+              <img src="media/twig.png" title="Twig" alt="Twig">
+            </div>
+            <div class="slide">
+              <img src="media/bootstrap.png" title="Bootstrap" alt="Bootstrap">
+            </div> 
+            <div class="slide">
+              <img src="media/mysql.png" title="Mysql" alt="Mysql">
+            </div> 
+            <div class="slide">
+              <img src="media/vsc.png" title="Visual studio code" alt="Visual studio code">
+            </div>
+            <div class="slide">
+              <img src="media/Sublim_text.png" title="Sublim text" alt="Sublim text">
+            </div>
+            <div class="slide">
+              <img src="media/github.png" title="Github" alt="Github">
+            </div>
+            <div class="slide">
+              <img src="media/docker.png" title="Docker" alt="Docker">
+            </div>
+          </div>
+		</section>
+
+        <a id="myqualities"></a>
+        <div id="quality">
+            <div class="space-title">
+                <h3>Qualités</h3>
+                <p>Parmi de nombreuses qualités nécessaires pour devenir un bon développeur et graphiste, 
+                il y en a 3 que je retiens et que je pense avoir :</p>
+            </div>
+
+            <div id="box">
+                <div class="each-box">
+                    <img src="media/passion.png" alt="icône de coeur du site flaticon" height="70" width="70" title="icône de coeur du site flaticon"> <!-- https://www.flaticon.com/premium-icon/like_2030957?term=heart&page=1&position=1&page=1&position=1&related_id=2030957&origin=search -->
+                    <h5>Passion</h5>
+                    <div class="box-content">
+                        <p>Etre passioné par ce que nous pratiquons c'est très important pour pouvoir offrir <strong class="pink">le meilleur résultat aux clients</strong> car ce dernier reflète notre motivation et notre implication.</p>
+                    </div>
+                </div>
+
+                <div class="each-box">
+                    <img src="media/one.png" alt="icône de l'index levé vers le haut du site flaticon" title="icône de l'index levé vers le haut du site flaticon" height="70" width="70"> <!-- https://www.flaticon.com/free-icon/one_1612732?term=finger%20one&page=1&position=5&page=1&position=5&related_id=1612732&origin=search -->
+                    <h5>Autonomie</h5>
+                    <div class="box-content">
+                        <p>Il est impossible de faire appel à un supérieur de manière permanente. En effet, nous serons amenés à réaliser des projets et donc à résoudre des problèmes seuls ce qui nous apporte une qualité supplémentaire : <strong class="pink">l'adaptation</strong> (aux différentes situations). </p>
+                    </div>
+                </div>
+
+                <div class="each-box">
+                    <img src="media/volume-up-interface-symbol.png" alt="icône d'un volume élevé du site flaticon" title="icône d'un volume élevé du site flaticon" height="70" width="70"> <!-- https://www.flaticon.com/free-icon/volume-up-interface-symbol_25695?term=volume%20up&page=1&position=1&page=1&position=1&related_id=25695&origin=search -->
+                    <h5>Ecoute</h5>
+                    <div class="box-content">
+                        <p>Un bon développeur ou graphiste est celui qui est à l'écoute de ses clients. <strong class="pink">S'ils sont satisfaits, ils reviendront vers nous et nous recommanderont</strong> : les écouter et répondre à leurs demandes sont donc des qualités indispensables.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+      <section id="certification">
+        <div class="certification-logo">
+        	<img src="media/medal.png" alt="icône d'une certification sur flaticon" title="icône d'une certification sur flaticon" height="210" width="210"> <!-- https://www.flaticon.com/free-icon/medal_1895474?term=certification&page=1&position=1&origin=search&related_id=1895474 -->
+        </div>
+        <div class="content-certification">
+            <p>Vous pouvez retrouver les certifications que j'ai obtenu lors de mes formations notamment intensive en webmaster, marketing digital ainsi que webdesign.</p> 
+            <button><a href="https://eportfolio-csiham.mssiweb.com/certifications">Découvrez mes certifications</a></button>
+        </div>
+      </section>
+
+        <a id="mywork"></a>
+        <div id="part-slider">
+            <div class="space-title">
+                <h3>Graphisme</h3>
+              <p>Lors de ma deuxième année du DEUST Webmaster et métiers de l'internet, j'ai réalisé un projet avec des collègues qui consistait à à réaliser un cahier 
+                des charges pour un artisan de chocolats et de confiseries personnalisables. Lors de la soutenance, pour leur présenter le projet que nous sommes prêts
+                à leur réaliser, nous avons créé un diaporama que vous pouvez retrouver juste <a href="/UEL202/Diapo_gp1.pdf" title="diaporama de notre projet">ici</a>.
+              </p>
+                <p>Vous pouvez retrouver également à travers ce slide, un logo (sous ses différentes déclinaisons) que j'ai réalisé lors d'une formation intensive (en ligne) 
+                chez <a href="https://enmodeformation.com/" target="_blank" title="Site du centre de formation en ligne 'En Mode Formation'">En Mode Formation</a></p>
+            </div>
+        <div class="part-slider">
+            <div class="part-slides">
+                <div class="part-slide"><img src="media/lebarista.jpg" alt="Déclinaisons d'un logo réalisé en formation externe" title="Déclinaisons d'un logo réalisé en formation externe"></div> <!-- https://smartmockups.com/fr/mockup/irIZTNa8R -->
+                <div class="part-slide"><img src="media/lebarista2.jpg" alt="Déclinaisons d'un logo réalisé en formation externe" title="Déclinaisons d'un logo réalisé en formation externe"></div> <!-- https://smartmockups.com/fr/mockup/QjsBo306j -->
+                <div class="part-slide"><img src="media/lebarista3.jpg" alt="Déclinaisons d'un logo réalisé en formation externe" title="Déclinaisons d'un logo réalisé en formation externe"></div> <!-- https://smartmockups.com/fr/mockup/pZpnDIeNH -->
+            </div>
+        </div>
+        </div>
+<?php include'includes/portfolio.html'; ?>
+<?php include'includes/footer.html'; ?>
